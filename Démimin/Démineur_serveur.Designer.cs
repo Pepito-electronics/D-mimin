623e75b83c59d0086f62f14c3d42ea43247d7cf0
Juan ALVAREZ LOPEZ <58698@etu.he2b.be>
2022-06-15 02:02:40 +0200

    deminmin

--- Démimin/Démineur_serveur.Designer.cs
+++ Démimin/Démineur_serveur.Designer.cs
@@ -52,9 +52,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(85, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 46);
+            this.label1.Size = new System.Drawing.Size(207, 46);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Déminmim";
+            this.label1.Text = "Déminmin";
             // 
             // textBox1
             // 
